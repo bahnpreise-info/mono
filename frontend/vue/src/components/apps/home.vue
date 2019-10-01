f89baaedfbd2828f75ce80a159e772f9ab089ebe
Stored <henry.brettschneider@nitrado.net>
2019-10-01 22:17:58 +0200

    Added €

--- frontend/vue/src/components/apps/home.vue
+++ frontend/vue/src/components/apps/home.vue
@@ -104,7 +104,7 @@
                             <div class="row no-gutters align-items-center">
                                 <div class="col mr-2">
                                     <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Durchschnittlicher Ticketpreis</div>
-                                    <div class="h5 mb-0 font-weight-bold text-gray-800" id="averageCosts">{{this.stats.data.globalaverageprice}}</div>
+                                    <div class="h5 mb-0 font-weight-bold text-gray-800" id="averageCosts">{{this.stats.data.globalaverageprice}}€</div>
                                 </div>
                                 <div class="col-auto"> <i class="fas fa-coins fa-2x text-gray-300"></i> </div>
                             </div>
