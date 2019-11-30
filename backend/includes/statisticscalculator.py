5ea3a87fad3b9e84908a9cf44eae5671e957ee76
Cubicroot <alex@cubicroot.xyz>
2019-11-30 17:03:59 +0100

    commenting

--- backend/includes/statisticscalculator.py
+++ backend/includes/statisticscalculator.py
@@ -2,8 +2,10 @@ from includes import mysql
 import datetime, configparser, logging, time, pytz, redis, json, math
 from orator import DatabaseManager
 
+#this line is not needed anymore i think
 #os.chdir("/opt/app") #change this according to your needs - working directory
 
+# setup database connection, read from config file
 config = configparser.ConfigParser()
 config.read_file(open('database.ini'))
 oratorconfig = {
@@ -19,6 +21,7 @@ oratorconfig = {
 db = DatabaseManager(oratorconfig)
 r = redis.Redis(host="redis")
 
+# this class is responsible for calculating all the statistics and sending them to the redis caching server
 class StatisticsCalculator():
     def __init__(self):
         self.status = {}    #saves the status (true/false) of important jobs
@@ -35,6 +38,7 @@ class StatisticsCalculator():
                 self.logger.info("Finished calculating statistics")
                 time.sleep(60)
 
+    # setup a logger
     def setupLogger(self):
         try:
             self.logger = logging.getLogger('StatisticsCalculator')
@@ -51,41 +55,51 @@ class StatisticsCalculator():
             print(e)
             return False
 
+    # calculate the statistics
     def calculate(self):
         data={}
         self.logger.debug("Getting stats from database")
-        self.logger.debug("Getting Requests in past hour")
+
+        # get amount of prices from the last hour
+        self.logger.debug("Getting Requests from past hour")
         query = "SELECT * FROM bahn_monitoring_prices WHERE bahn_monitoring_prices.time >= DATE_SUB(NOW(), INTERVAL 1 HOUR)"
         result = db.select(query)
         data["hourlyrequests"] = len(result)
 
+        # get amount of prices from the last 24 hours
         self.logger.debug("Getting Requests within the last day")
         query = "SELECT * FROM bahn_monitoring_prices WHERE bahn_monitoring_prices.time >= DATE_SUB(NOW(), INTERVAL 1 DAY)"
         result = db.select(query)
         data["dailyrequests"] = len(result)
 
+        # get the amount of stations available
         self.logger.debug("Getting station count")
         query = "SELECT * FROM bahn_monitoring_stations"
         result = db.select(query)
         data["stationcount"] = len(result)
 
+        # get amount of all connections from the database
         self.logger.debug("Getting connection count")
         query = "SELECT * FROM bahn_monitoring_connections"
         result = db.select(query)
         data["connections"] = len(result)
 
+        # get amount of all active monitored connections
         self.logger.debug("Getting active connection count")
         query = "SELECT * FROM bahn_monitoring_connections WHERE active = 1"
         result = db.select(query)
         data["activeconnections"] = len(result)
 
+        # get average price from all connections
         self.logger.debug("Getting average price")
         query = "SELECT ROUND(AVG(bahn_monitoring_prices.price), 2) as average FROM bahn_monitoring_connections INNER JOIN bahn_monitoring_prices on (bahn_monitoring_connections.id = bahn_monitoring_prices.connection_id)"
         result = db.select(query)
         data["globalaverageprice"] = result[0]["average"]
 
+        # calculate statistics by days to departure
         self.logger.debug("Getting minimum and maximum prices for each days before departure")
         days_to_prices = {}
+        # these dicts holds one statistical value for each day
         days_to_minimum_prices = {}
         days_to_maximum_prices = {}
         days_to_average_prices = {}
@@ -108,19 +122,19 @@ class StatisticsCalculator():
             for price in prices:
                 i += 1
                 sum_ += float(price)
-                if float(price) < float(minimum) and float(price) > threshold:
+                if float(price) < float(minimum) and float(price) > threshold:  # minimum daily price
                     minimum = price
-                if float(price) > float(maximum) and float(price) > threshold:
+                if float(price) > float(maximum) and float(price) > threshold:  # maximum daily price
                     maximum = price
             days_to_minimum_prices[day] = minimum
             days_to_maximum_prices[day] = maximum
-            days_to_average_prices[day] = round(sum_/i, 2)
+            days_to_average_prices[day] = round(sum_/i, 2)  # average daily price
 
             stdDev = 0
             i = 0
             for price in prices:
                 i += 1
-                stdDev += math.pow(days_to_average_prices[day]-float(price), 2)
+                stdDev += math.pow(days_to_average_prices[day]-float(price), 2) # deviation for each day
             days_to_deviation_prices[day] = round(math.sqrt(stdDev/i), 2)
 
         data["days_to_average_prices"] = {}
@@ -129,22 +143,22 @@ class StatisticsCalculator():
         data["days_to_average_prices"] = days_to_average_prices
         data["days_to_deviation_prices"] = days_to_deviation_prices
 
+        # calculate statistics by weekday (0-6 => Monday-Sunday)
         self.logger.debug("Getting average price per weekday")
+
         data["prices_to_weekdays"] = {}
         data["prices_to_weekdays_stdev"] = {}
         map = {0 : "monday", 1 : "tuesday", 2 : "wednesday", 3 : "thursday", 4 : "friday", 5 : "saturday", 6 : "sunday"}
         query = "SELECT ROUND(AVG(bahn_monitoring_prices.price), 2) as average, WEEKDAY(bahn_monitoring_prices.time) AS weekday from bahn_monitoring_prices GROUP by weekday"
         results = db.select(query)
         for result in results:
-            data["prices_to_weekdays"][map[result["weekday"]]] = result["average"]
+            data["prices_to_weekdays"][map[result["weekday"]]] = result["average"]  # average price by weekday
 
         self.logger.debug("Getting  standard deviation price per weekday")
         query = "SELECT ROUND(STD(bahn_monitoring_prices.price), 2) as average, WEEKDAY(bahn_monitoring_prices.time) AS weekday from bahn_monitoring_prices GROUP by weekday"
         results = db.select(query)
         for result in results:
-            data["prices_to_weekdays_stdev"][map[result["weekday"]]] = result["average"]
+            data["prices_to_weekdays_stdev"][map[result["weekday"]]] = result["average"]    # deviation by weekday       
 
-        
-
-
+        # send everything to the redis cache, so the api can access it
         r.setex("stats_data", datetime.timedelta(minutes=5), value=json.dumps(data))
