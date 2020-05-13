72bf4eb2d4113d2f9e2ebd00c66e9284f7978c7e
henry <henry@stored.cc>
2020-05-13 21:12:13 +0200

    Pool workers

--- backend/backend.py
+++ backend/backend.py
@@ -1,7 +1,9 @@
 import _thread, time, os, redis
-from includes import scheduler, connectionmanager, statistic_cacher, tracks_cacher, connections_cacher
+from includes import scheduler, connectionmanager, statistic_cacher
+from includes.functions import Logger, ConnectionPrices, TrackPrices, Offset
 from configparser import ConfigParser
 from orator import DatabaseManager
+from multiprocessing import Pool
 
 # setup database connection, read from config file
 config = ConfigParser()
@@ -17,14 +19,44 @@ oratorconfig = {
   }
 }
 
+#Logger instance
+logger = Logger("Backend")
+
+#Orator instance
 db = DatabaseManager(oratorconfig)
+
+#Redis instance
 r = redis.Redis(host=config.get('redis', 'host'))
 
+#These can run single threaded
 _thread.start_new_thread( scheduler.Scheduler, () )
 _thread.start_new_thread( connectionmanager.ConnectionManager, ())
 _thread.start_new_thread( statistic_cacher.StatisticsCalculator, (db, r))
-_thread.start_new_thread( tracks_cacher.TracksCacher, (db, r,))
-_thread.start_new_thread( connections_cacher.ConnectionsCacher, (db, r,))
+
+#connection cache function
+def connection_cache(connection):
+  logger.write("Processing {0} -> {1} @ {2}".format(connection["start"], connection["end"], connection["starttime"]))
+  connectiondb = ConnectionPrices(db, connection["id"], r)
+  connectiondb.getAggregatedData() #we don't need the result, but the function refreshed the cache
+
+#track cache function
+def track_cache(track):
+  logger.write("Processing {0} -> {1}".format(track["start"], track["end"]))
+  trackdb = TrackPrices(db, track, r)
+  trackdb.getAggregatedData() #we don't need the result, but the function refreshed the cache
 
 while True:
-  time.sleep(10)
+  try:
+    #Pool worker
+    pool = Pool(4)
+    #cache connections
+    for connection in db.select("SELECT * FROM bahn_monitoring_connections where active = 1"):
+      pool.apply_async(connection_cache, (connection,))
+
+    #cache tracks
+    for track in db.select("SELECT DISTINCT start, end FROM bahn_monitoring_connections WHERE active = 1"):
+      pool.apply_async(track_cache, (track,))
+    pool.close()
+    pool.join()
+  except:
+    logger.write("Something went wrong, shrug")
